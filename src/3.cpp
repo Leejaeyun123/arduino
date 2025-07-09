d2b092fab355f50d1700be0b4a95a9237cab7373
Leejaeyun123 <qazxczf@gmail.com>
2025-07-09 10:22:12 +0900

    07-09

--- src/3.cpp
+++ src/3.cpp
@@ -1,15 +1,15 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT); // 보통 D4에 연결된 내장 LED
-  Serial.begin(9600);
-}
+// void setup() {
+//   pinMode(LED_BUILTIN, OUTPUT); // 보통 D4에 연결된 내장 LED
+//   Serial.begin(9600);
+// }
 
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH); // LED ON
-  Serial.println(HIGH);
-  delay(1000); // 1초 대기
-  digitalWrite(LED_BUILTIN, LOW);  // LED OFF
-  Serial.println(LOW);
-  delay(1000); // 1초 대기
-}
+// void loop() {
+//   digitalWrite(LED_BUILTIN, HIGH); // LED ON
+//   Serial.println(HIGH);
+//   delay(1000); // 1초 대기
+//   digitalWrite(LED_BUILTIN, LOW);  // LED OFF
+//   Serial.println(LOW);
+//   delay(1000); // 1초 대기
+// }
